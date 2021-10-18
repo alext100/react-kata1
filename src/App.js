ab6cc7162b7bb6adb11403bfbd7da8304f0a9d14
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-18 17:22:57 +0200

    In App.js circle.map() added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Circle from './Components/Circle';
 
 function App() {
-  return (
-    <div className="App">
-      <Circle />
-    </div>
-  );
+    const circle = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+
+    return (
+        <div className="App">
+            {circle.map((circle) => < Circle key={circle.id} />)}
+        </div>
+    );
 }
 
 export default App;
